3f577e253ab6ceec2100229641452cf01bc60244
mbishr <yunismahmoud@gmail.com>
2020-02-28 10:32:33 +0200

    Add files via upload

---
+++ Java_hello_world.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_hello_world;
+
+public class java_hello_world {
+
+    
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=10;i++){
+            System.out.println("Hello Mahmoud!"+i);
+    }
+    }
+        
+    
+}
+
